5b01f042b10c660020e475abd67782b830890b63
Greg-Smith209 <gms52@sunyorange.edu>
2022-11-15 10:43:23 -0500

    progress apply done

---
+++ analyze apply extend/apply/scripts/script.js
@@ -0,0 +1,14 @@
+/*
+Gregory Smith
+11/15/2022
+script.js
+*/
+// Function to display content
+funtion content(){
+  var text = document.getElementById("new");
+  text.textContent = "Hi. I am a new paragraph the was created with a JavaScript
+  funtion.";
+  text.style.color = "#c0145f";
+  text.style.fontSize = "2em";
+  
+}
