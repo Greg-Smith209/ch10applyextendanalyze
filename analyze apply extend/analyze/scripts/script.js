7e5ef2bd5391013711e060d1a3d043bedd5a6da7
Greg-Smith209 <gms52@sunyorange.edu>
2022-11-18 13:31:05 -0500

    done

--- analyze apply extend/analyze/scripts/script.js
+++ analyze apply extend/analyze/scripts/script.js
@@ -1,12 +1,37 @@
 /*
-    Student Name:
+    Student Name:Gregory Smith
     File Name: script.js
-    Date: 
+    Date: 11/18/2022
 */
 
 //Function to display the first picture
-function pic1 {
-    imgSource.src = "images/trunk-bay.jpg"
-    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block
+function pic1 (){
+    imgSource.src = "images/trunk-bay.jpg";
+    imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block";
     figCap.textContent = "Trunk Bay in St. John";
+    var figElement = document.getElementById("placeholder");
+    var imgSource = document.getElementById("image");
+    var figCap = document.querySelector("figcaption");
+}
+// fun to display the second picture
+
+function pic2 (){
+  imgSource.src = "images/sanjuan.jpg";
+  imgSource.alt = "Elevated view of San Juan coast";
+  figElement.style.display = "block";
+  figCap.textContent = "Coast of San Juan";
+  var figElement = document.getElementById("placeholder");
+  var imgSource = document.getElementById("image");
+  var figCap = document.querySelector("figcaption");
+}
+// Function to display the thrid picture
+function pic1 (){
+    imgSource.src = "images/curacao.jpg";
+    imgSource.alt = "The blue waters of Curacao";
+    figElement.style.display = "block";
+    figCap.textContent = "Curacao";
+    var figElement = document.getElementById("placeholder");
+    var imgSource = document.getElementById("image");
+    var figCap = document.querySelector("figcaption");
+}
